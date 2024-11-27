7c04e347e45055abd68edf2adc8603dd5d1a9981
claraharguindey <clarisaharguindey@gmail.com>
2024-11-27 13:00:10 +0100

    Styles

--- sketch.js
+++ sketch.js
@@ -1,18 +1,17 @@
 function setup() {
-    let canvas = createCanvas(windowWidth / 2, windowHeight);
-    canvas.parent("canvas-side"); 
-    background(255);
+  let canvas = createCanvas(windowWidth / 2 , windowHeight - 22);
+  canvas.parent("canvas-side");
+  background(255);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    stroke(0);
+    strokeWeight(1);
+    line(pmouseX, pmouseY, mouseX, mouseY);
   }
-  
-  function draw() {
-    if (mouseIsPressed) {
-      stroke(0); 
-      strokeWeight(1);
-      line(pmouseX, pmouseY, mouseX, mouseY); 
-    }
-  }
-  
-  function windowResized() {
-    resizeCanvas(windowWidth / 2, windowHeight);
-  }
-  
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth / 2, windowHeight - 22);
+}
